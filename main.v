b273fe8bbc80345e37327d86e45bd16a063a4fe5
Steve Phillips <steve@tryingtobeawesome.com>
2020-11-08 10:29:14 -0800

    main.v: `or { panic(err) }` -> `?` (#1)

--- main.v
+++ main.v
@@ -6,20 +6,11 @@ const (
 
 fn main() {
 	dm := frida.new_device_manager()
-	device := dm.get_device_by_type_sync (frida.device_type_usb) or {
-		panic (err)
-	}
-	session := device.attach_sync (pid) or {
-		panic(err)
-	}
-	script := session.create_script_sync(code) or {
-		panic(err)
-	}
+	device := dm.get_device_by_type_sync(frida.device_type_usb)?
+	session := device.attach_sync(pid)?
+	script := session.create_script_sync(code)?
 	script.on_message(on_message)
 	session.on_detached(on_detach)
 
-	script.load_sync() or {
-		panic(err)
-	}
+	script.load_sync()?
 }
-
