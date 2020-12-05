87536da53504d91e7f0dcc9b876c107e5d54c0f8
pancake <pancake@nopcode.org>
2020-12-06 00:29:18 +0100

    Fix linking

--- frida/host/lib.v
+++ frida/host/lib.v
@@ -1,7 +1,8 @@
 module host
 
 #flag -Iext/frida
-#flag ext/frida/libfrida-core.a -lresolv -framework Foundation -lbsm -framework AppKit
+#flag -Lext/frida
+#flag -lfrida-core -lresolv -framework Foundation -lbsm -framework AppKit
 
 #include <frida-core.h>
 
